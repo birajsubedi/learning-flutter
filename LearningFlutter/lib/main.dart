8a0845c03413eff8e9390d32c0a97154adb71f43
Biraj Subedi <braj.me121@gmail.com>
2020-09-02 17:41:51 +0545

    Rows

--- LearningFlutter/lib/main.dart
+++ LearningFlutter/lib/main.dart
@@ -10,17 +10,32 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      drawer: Drawer(
-        child: Text('hello'),
-      ),
-      appBar: AppBar(
-        title: Text('Learning Flutter Basics'),
-      ),
-      body: Container(
-        padding: EdgeInsets.all(90),
-        color: Colors.blue[250],
-        child: Text('Birja'),
-      ),
-    );
+        drawer: Drawer(
+          child: Text('hello'),
+        ),
+        appBar: AppBar(
+          title: Text('Learning Flutter Basics'),
+        ),
+        body: Row(
+          mainAxisAlignment:
+              MainAxisAlignment.spaceEvenly, // horizontal allignment
+          crossAxisAlignment: CrossAxisAlignment.center, // vertical allignment
+          children: <Widget>[
+            Text('Biraj'),
+            FlatButton(
+              onPressed: () {},
+              color: Colors.black,
+              child: Text(
+                'Click me',
+                style: TextStyle(color: Colors.white, fontSize: 30),
+              ),
+            ),
+            Container(
+              child: Text('roll'),
+              color: Colors.amber,
+              padding: EdgeInsets.all(10),
+            ),
+          ],
+        ));
   }
 }
